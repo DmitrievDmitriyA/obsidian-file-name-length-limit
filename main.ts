d2eec51f5e6313c9c433be6d6655a4ac751ed7dc
Dmitrii Dmitriev <dmitriy.dmitriev.aleks@gmail.com>
2024-03-03 00:39:24 +0100

    Add file name checks and command to check all files

--- main.ts
+++ main.ts
@@ -1,69 +1,104 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 interface FileNameLengthLimitPluginSettings {
-	lengthLimit: number;
+    lengthLimit: number;
 }
 
 const DEFAULT_SETTINGS: FileNameLengthLimitPluginSettings = {
-	lengthLimit: 255
+    lengthLimit: 255
 }
 
 export default class FileNameLengthLimitPlugin extends Plugin {
-	settings: FileNameLengthLimitPluginSettings;
+    settings: FileNameLengthLimitPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new FileNameLengthLimitSettingTab(this.app, this));
+        this.addSettingTab(new FileNameLengthLimitSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // Check file length when a new file becomes active
+        this.registerEvent(
+            this.app.workspace.on('file-open', (file: TFile) => {
+                this.checkFileLength(file);
+            })
+        );
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // Check file length when the active file's name is changed
+        this.registerEvent(
+            this.app.vault.on('rename', (file: TFile) => {
+                if (file === this.app.workspace.getActiveFile()) {
+                    this.checkFileLength(file);
+                }
+            })
+        );
 
-	onunload() {
+		// Add a command to check all files and generate a report
+        this.addCommand({
+            id: 'check-all-file-names',
+            name: 'Check All File Names',
+            callback: () => this.checkAllFileNames(),
+        });
+    }
 
-	}
+    checkFileLength(file: TFile) {
+        const length = file.path.length;
+        if (length > this.settings.lengthLimit) {
+            new Notice(`File length exceeds the limit! Length: ${length}, Limit: ${this.settings.lengthLimit}`);
+        }
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	async checkAllFileNames() {
+        const filesWithLongNames: string[] = [];
+        this.app.vault.getFiles().forEach(file => {
+            if (file.path.length > this.settings.lengthLimit) {
+                filesWithLongNames.push(file.path);
+            }
+        });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        if (filesWithLongNames.length > 0) {
+            const reportContent = filesWithLongNames.map(path => `- [[${path}]]`).join('\n');
+            const reportFile = await this.app.vault.create('FilesWithTooLongNames.md', reportContent);
+            new Notice(`Report created: ${reportFile.path}`);
+        } else {
+            new Notice('No files with names exceeding the length limit.');
+        }
+    }
+
+    onunload() {
+
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class FileNameLengthLimitSettingTab extends PluginSettingTab {
-	plugin: FileNameLengthLimitPlugin;
+    plugin: FileNameLengthLimitPlugin;
 
-	constructor(app: App, plugin: FileNameLengthLimitPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: FileNameLengthLimitPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('File name length limit')
-			.setDesc('The limit is set for the file name including path')
-			.addText(string => string
-				.setPlaceholder('Enter the limit')
-				.setValue(this.plugin.settings.lengthLimit.toString())
-				.onChange(async (value) => {
-					this.plugin.settings.lengthLimit = Number(value);
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('File name length limit')
+            .setDesc('The limit is set for the file name including path')
+            .addText(text => text
+                .setPlaceholder('Enter the limit')
+                .setValue(this.plugin.settings.lengthLimit.toString())
+                .onChange(async (value) => {
+                    this.plugin.settings.lengthLimit = Number(value);
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
