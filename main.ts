7178a21f124804d439ac476f3360c59a14a0a01f
Dmitrii Dmitriev <dmitriy.dmitriev.aleks@gmail.com>
2024-03-17 16:53:37 +0100

    Added the current length to the status bar

--- main.ts
+++ main.ts
@@ -2,37 +2,40 @@ import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian'
 
 interface FileNameLengthLimitPluginSettings {
     lengthLimit: number;
+    showStatusBar: boolean;
 }
 
 const DEFAULT_SETTINGS: FileNameLengthLimitPluginSettings = {
-    lengthLimit: 144
-}
+    lengthLimit: 144,
+    showStatusBar: true
+};
 
 export default class FileNameLengthLimitPlugin extends Plugin {
     settings: FileNameLengthLimitPluginSettings;
+    statusBarEl?: HTMLElement;
 
     async onload() {
         await this.loadSettings();
 
         this.addSettingTab(new FileNameLengthLimitSettingTab(this.app, this));
+        this.updateStatusBarVisibility();
 
-        // Check file length when a new file becomes active
         this.registerEvent(
             this.app.workspace.on('file-open', (file: TFile) => {
                 this.checkFileLength(file);
+                this.updateStatusBar();
             })
         );
 
-        // Check file length when the active file's name is changed
         this.registerEvent(
             this.app.vault.on('rename', (file: TFile) => {
                 if (file === this.app.workspace.getActiveFile()) {
                     this.checkFileLength(file);
+                    this.updateStatusBar();
                 }
             })
         );
 
-		// Add a command to check all files and generate a report
         this.addCommand({
             id: 'check-all-file-names',
             name: 'Check all file names',
@@ -47,7 +50,7 @@ export default class FileNameLengthLimitPlugin extends Plugin {
         }
     }
 
-	async checkAllFileNames() {
+    async checkAllFileNames() {
         const filesWithLongNames: string[] = [];
         this.app.vault.getFiles().forEach(file => {
             if (file.path.length > this.settings.lengthLimit) {
@@ -64,8 +67,24 @@ export default class FileNameLengthLimitPlugin extends Plugin {
         }
     }
 
-    onunload() {
+    updateStatusBar() {
+        if (this.settings.showStatusBar && this.statusBarEl) {
+            const file = this.app.workspace.getActiveFile();
+            const length = file ? file.path.length : 0;
+            this.statusBarEl.setText(`Length: ${length}`);
+        }
+    }
 
+    updateStatusBarVisibility() {
+        if (this.settings.showStatusBar) {
+            if (!this.statusBarEl) {
+                this.statusBarEl = this.addStatusBarItem();
+            }
+            this.updateStatusBar();
+        } else {
+            this.statusBarEl?.remove();
+            this.statusBarEl = undefined;
+        }
     }
 
     async loadSettings() {
@@ -74,6 +93,11 @@ export default class FileNameLengthLimitPlugin extends Plugin {
 
     async saveSettings() {
         await this.saveData(this.settings);
+        this.updateStatusBarVisibility();
+    }
+
+    onunload() {
+        this.statusBarEl?.remove();
     }
 }
 
@@ -98,6 +122,16 @@ class FileNameLengthLimitSettingTab extends PluginSettingTab {
                 .setValue(this.plugin.settings.lengthLimit.toString())
                 .onChange(async (value) => {
                     this.plugin.settings.lengthLimit = Number(value);
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName('Show Length in Status Bar')
+            .setDesc('Toggle the visibility of the file name length in the status bar.')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.showStatusBar)
+                .onChange(async (value) => {
+                    this.plugin.settings.showStatusBar = value;
                     await this.plugin.saveSettings();
                 }));
     }
